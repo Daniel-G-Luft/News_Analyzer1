a4d403163e88e6e3358e7bb5a58d6e3c24a06b13
Daniel-G-Luft <132319722+Daniel-G-Luft@users.noreply.github.com>
2023-05-02 20:44:58 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
-from main import analyze_article, answer_question, custom_prompt, llm
-import openai
+from main import analyze_article, answer_question, get_llm
+import json
 
 app = Flask(__name__)
 
@@ -10,10 +10,13 @@ def index():
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
-    article_url = request.form.get('article_url')
-    if article_url:
+    data = request.get_json()
+    article_url = data.get('article_url')
+    api_key = data.get('api_key')
+
+    if article_url and api_key:
         try:
-            title, source, date, author, analysis, doc = analyze_article(article_url, custom_prompt)
+            title, source, date, author, analysis, doc = analyze_article(article_url, api_key)
             result = {
                 'title': title,
                 'source': source,
@@ -25,31 +28,20 @@ def analyze():
         except Exception as e:
             return jsonify({"error": str(e)})
     else:
-        return jsonify({"error": "Please enter a URL."})
+        return jsonify({"error": "Please enter a URL and an API key."})
 
 @app.route('/ask_question', methods=['POST'])
 def ask_question():
-    question = request.form.get('question')
-    doc_text = request.form.get('doc_text')
+    data = request.get_json()
+    question = data.get('question')
+    doc_text = data.get('doc_text')
+    api_key = data.get('api_key')
 
-    if not question or not doc_text:
-        return jsonify({"error": "Please provide a question and the document text."})
+    if not question or not doc_text or not api_key:
+        return jsonify({"error": "Please provide a question, the document text, and an API key."})
 
     try:
-        # Combine the document text and the question
-        prompt = f"{doc_text}\n\nQuestion: {question}\nAnswer:"
-
-        # Use the OpenAI API to get the answer
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=100,
-            n=1,
-            stop=None,
-            temperature=0.5,
-        )
-
-        answer = response.choices[0].text.strip()
+        answer = answer_question(question, doc_text, api_key)
         return jsonify({"answer": answer})
     except Exception as e:
         print(e)  # Log the error for debugging
